74384539b477a04a8db70771553aaa29bbea924d
atoups2202 <56981651+atoups2202@users.noreply.github.com>
2019-10-31 11:35:05 -0500

    Add files via upload

---
+++ AlexToups_TakeHome3.R
@@ -0,0 +1,23 @@
+
+rm(list = ls())
+cat("\014")  
+
+
+#---------------------------------------------------------------------------------------------------#
+#                                                                                                   #
+#                                                                                                   #
+#     Take Home Assignment 3                                                                        #
+#         A. Toups October 31st 2019                                                                #
+#                                                                                                   #
+#   Description :Script will provide the answers to Take Home Assignent 3                           #                                                           #
+#                                                                                                   #
+#   Dependencies: Script requires use of the tidyverse.                                             #
+#-----------------------------
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+
+typeof(diamonds)
+diamonds[1:6,]
